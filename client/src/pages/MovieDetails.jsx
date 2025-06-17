c659cfa49cd22b97b8841477a15ddbc21d376227
Schrodinger-2027 <Schrodinger2027@gmail.com>
2025-06-17 18:48:05 +0530

    new feature

--- client/src/pages/MovieDetails.jsx
+++ client/src/pages/MovieDetails.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { dummyDateTimeData, dummyShowsData } from '../assets/assets'
 
 const MovieDetails = () => {
-  return (
-    <div>
+  const {id} = useParams()
+  const [shows , setshows] = useState(null)
+
+  const getshow = async () => {
+    const show = dummyShowsData.find(show => show._id === id)
+    setshows({
+      movie: show,
+      dataTime: dummyDateTimeData
+    })
+  }
+
+  useEffect(()=>{
+    getshow()
+  }, [id])
+
+  return shows ? (
+    <div className='px-6 md:px-16 lg:px-40 pt-30 md:pt-50'>
       
     </div>
+  ) : (
+    <div>Loading Data ......</div>
   )
 }
 
