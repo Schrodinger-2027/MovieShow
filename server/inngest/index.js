20acc5b5fa984a18e45e5ca85bc6fd6ce0f3ac3f
Schrodinger-2027 <Schrodinger2027@gmail.com>
2025-06-25 10:17:57 +0530

    crazy

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,5 +1,5 @@
 import { Inngest } from "inngest";
-import User from "../models/UserModel.js";
+import User from "../models/User.js";
 
 export const inngest = new Inngest({ id: "movie-ticket-booking" });
 
@@ -49,5 +49,5 @@ const syncUserUpdation = inngest.createFunction(
 export const functions = [
   syncUserCreation,
   syncUserDeletion,
-  syncUserUpdation,
+  syncUserUpdation
 ];
